1c2ad75500cf7bc44dc1d2978c281fe87c94e671
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2023-10-05 20:38:34 +0300

    add make_titled function

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
-def make_upper(string):
-    return string.upper()
+def make_capitals(text: str):
+    """Makes letters in the text capital"""
+    return text.upper()
+
+
+def make_titled(text: str):
+    """Makes the 1st letter in each word capital"""
+    return text.title()
+
